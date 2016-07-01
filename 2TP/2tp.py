58048136298c2eaff6ef1156bf98eadf2ace0029
Xunnamius (Babylon) <Xunnamius@users.noreply.github.com>
2016-07-01 06:24:47 -0500

    better comments and output names

--- 2TP/2tp.py
+++ 2TP/2tp.py
@@ -18,26 +18,30 @@ if __name__ == "__main__":
 
     imgDimByteCount = int(squareImgDim / 8)
 
-    # Generate uniform random key for XOR from random bytes
-
+    # Generate uniform random key for XOR from random bytes and generate an img
     keyData = os.urandom(imgDimByteCount * squareImgDim)
     keyImg = Image.frombytes('1', (squareImgDim, squareImgDim), keyData)
 
+    # Save the results of the above operation
     keyImg.save('3. key.bmp')
 
-    # Do the XOR and save the results
+    # Do the XORs and save the results
 
     brokenData = broken.tobytes()
     cryptoData = crypto.tobytes()
 
+    # broken XOR key
     bXkData = bytes(map(operator.xor, brokenData, keyData))
+    # crypto XOR key
     cXkData = bytes(map(operator.xor, cryptoData, keyData))
+    # broken XOR key XOR crypto XOR key
     bXcData = bytes(map(operator.xor, bXkData, cXkData))
 
     brokenCipherImg = Image.frombytes('1', (squareImgDim, squareImgDim), bXkData)
     cryptoCipherImg = Image.frombytes('1', (squareImgDim, squareImgDim), cXkData)
     brokenCryptoCipherImg = Image.frombytes('1', (squareImgDim, squareImgDim), bXcData)
 
-    brokenCipherImg.save('4. broken-cipher.bmp')
-    cryptoCipherImg.save('5. crypto-cipher.bmp')
-    brokenCryptoCipherImg.save('6. broken-crypto-cipher.bmp')
+    # Save the results
+    brokenCipherImg.save('4. broken-xor-key.bmp')
+    cryptoCipherImg.save('5. crypto-xor-key.bmp')
+    brokenCryptoCipherImg.save('6. broken-xor-key-xor-crypto-xor-key.bmp')
