c23e4ef2b9652514893cadd7350fcaf83e4441fe
Xunnamius (Babylon) <Xunnamius@users.noreply.github.com>
2016-07-01 06:15:57 -0500

    working!

--- 2TP/2tp.py
+++ 2TP/2tp.py
@@ -3,21 +3,41 @@
 
 import os
 import math
+import operator
 from PIL import Image
 
 if __name__ == "__main__":
-    broken = Image.open('broken.png')
-    crypto = Image.open('crypto.png')
+    broken = Image.open('broken.bmp')
+    crypto = Image.open('crypto.bmp')
 
     assert broken.size == crypto.size
-    assert broken.size % 8 == 0
 
     squareImgDim = broken.size[0]
-    imgDimByteCount = squareImgDim / 8
 
-    # Generate random key for XOR
-    key = os.urandom(imgDimByteCount * squareImgDim)
-    keyImg = Image.frombytes('1', (squareImgDim, squareImgDim), key)
+    assert squareImgDim % 8 == 0
+
+    imgDimByteCount = int(squareImgDim / 8)
+
+    # Generate uniform random key for XOR from random bytes
+
+    keyData = os.urandom(imgDimByteCount * squareImgDim)
+    keyImg = Image.frombytes('1', (squareImgDim, squareImgDim), keyData)
+
+    keyImg.save('key.bmp')
 
     # Do the XOR and save the results
-    keyImg.save('key.bmp')
+
+    brokenData = broken.tobytes()
+    cryptoData = crypto.tobytes()
+
+    bXkData = bytes(map(operator.xor, brokenData, keyData))
+    cXkData = bytes(map(operator.xor, cryptoData, keyData))
+    bXcData = bytes(map(operator.xor, bXkData, cXkData))
+
+    brokenCipherImg = Image.frombytes('1', (squareImgDim, squareImgDim), bXkData)
+    cryptoCipherImg = Image.frombytes('1', (squareImgDim, squareImgDim), cXkData)
+    brokenCryptoCipherImg = Image.frombytes('1', (squareImgDim, squareImgDim), bXcData)
+
+    brokenCipherImg.save('broken-cipher.bmp')
+    cryptoCipherImg.save('crypto-cipher.bmp')
+    brokenCryptoCipherImg.save('broken-crypto-cipher.bmp')
